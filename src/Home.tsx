7fbe84e9e1468f9087ab11d6988801cb56ceaa39
lnrssll <lnrssll@pm.me>
2021-12-01 23:16:49 -0800

    first commit custom frontend

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,7 +2,19 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Countdown from "react-countdown";
 import { Button, CircularProgress, Snackbar } from "@material-ui/core";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import {
+  Videocam,
+  Collections,
+  Palette,
+  Group,
+  Public
+} from '@material-ui/icons';
 import Alert from "@material-ui/lab/Alert";
+
+import gif1 from './images1.gif';
+import gif2 from './images2.gif';
+import gif3 from './images3.gif';
 
 import * as anchor from "@project-serum/anchor";
 
@@ -19,13 +31,15 @@ import {
   shortenAddress,
 } from "./candy-machine";
 
-const ConnectButton = styled(WalletDialogButton)``;
+const ConnectButton = styled(WalletDialogButton)`
+  background-color: grey;
+`;
 
 const CounterText = styled.span``; // add your styles here
 
-const MintContainer = styled.div``; // add your styles here
-
-const MintButton = styled(Button)``; // add your styles here
+const MintButton = styled(Button)`
+  background-color: grey;
+`; // add your styles here
 
 export interface HomeProps {
   candyMachineId: anchor.web3.PublicKey;
@@ -166,24 +180,36 @@ const Home = (props: HomeProps) => {
   ]);
 
   return (
-    <main>
+    <div className="flex grid gap-8 p-12 items-center w-full">
+      <div className="h-12 w-full font-bold text-xl text-black">
+        MENACE CLUB NFT
+      </div>
       {wallet && (
-        <p>Wallet {shortenAddress(wallet.publicKey.toBase58() || "")}</p>
+        <div className="animate-bounce">
+          <div className="w-64 h-44 mt-10 pt-4 shadow-lg rounded-xl bg-black transform -rotate-6">
+            <div className="w-64 bg-blue-100 h-10" />
+            <div className="flex grid grid-cols-2">
+              <div className="pl-4 float-left">
+                <div className="mt-6">Mike Truck</div>
+                <div className="">{shortenAddress(wallet.publicKey.toBase58() || "", 5)}</div>
+                <div className="mt-2 text-xs">Valid Thru 12/21</div>
+              </div>
+              <div className="pr-4 float-right">
+                <div className="mt-4 text-right">MENACE</div>
+                <div className="text-xs text-right">Card</div>
+                <div className="mt-4 text-right">{(balance || 0).toLocaleString()} SOL</div>
+              </div>
+            </div>
+          </div>
+        </div>
       )}
 
-      {wallet && <p>Balance: {(balance || 0).toLocaleString()} SOL</p>}
-
-      {wallet && <p>Total Available: {itemsAvailable}</p>}
-
-      {wallet && <p>Redeemed: {itemsRedeemed}</p>}
-
-      {wallet && <p>Remaining: {itemsRemaining}</p>}
-
-      <MintContainer>
+      <div className="justify-self-center">
         {!wallet ? (
-          <ConnectButton>Connect Wallet</ConnectButton>
+          <ConnectButton className="bg-gray-400">Connect Wallet</ConnectButton>
         ) : (
           <MintButton
+            className=""
             disabled={isSoldOut || isMinting || !isActive}
             onClick={onMint}
             variant="contained"
@@ -206,7 +232,24 @@ const Home = (props: HomeProps) => {
             )}
           </MintButton>
         )}
-      </MintContainer>
+        {wallet && <div className="mt-2 text-sm text-center">1 SOL</div>}
+      </div>
+
+      {wallet && <div className="text-center">{itemsRemaining}/{itemsAvailable} remaining</div>}
+      
+      <div className="grid grid-cols-3 overflow-hidden">
+        <img src={gif1} alt="gif1" className="h-full" />
+        <img src={gif2} alt="gif2" className="" />
+        <img src={gif3} alt="gif3" className="h-full" />
+      </div>
+
+      <div className="grid grid-cols-5">
+        <SvgIcon component={Collections} className="place-self-center" />
+        <SvgIcon component={Videocam} className="place-self-center" />
+        <SvgIcon component={Public} className="place-self-center" />
+        <SvgIcon component={Palette} className="place-self-center" />
+        <SvgIcon component={Group} className="place-self-center" />
+      </div>
 
       <Snackbar
         open={alertState.open}
@@ -220,7 +263,7 @@ const Home = (props: HomeProps) => {
           {alertState.message}
         </Alert>
       </Snackbar>
-    </main>
+    </div>
   );
 };
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="react-scripts" />
+
+declare module "*.gif" {
+  const value: any;
+  export = value;
+}
